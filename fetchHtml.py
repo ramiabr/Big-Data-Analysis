991a1ba09b366b31a84d1f98988d8955b9d6c3d8
Ramkumar Subramanian <s.ramkumar.srm@gmail.com>
2017-08-24 21:24:32 -0700

    Add files via upload

--- fetchHtml.py
+++ fetchHtml.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python
+#
+# Description:  - This program Extracts Gold and Silver Prices from investing.com website 
+#		- As First Step the program builds MySQL database "goldSilverPrices" 
+#		- MySQL database will have two tables gold and silver which will store 
+#		  corresponding Price Data from website 
+#		- The website will be parsed using Xpath module
+#		- Parsed information is updated into database 
+#
+# Inputs     :  None
+# Outputs    :  MySQL database "goldSilverPrices" tables gold and silver
+# 
+# Dependancies:  This code requires below modules to be installed and available
+#		 - MySQLdb
+#		 - lxml
+#		 - requests
+#		 - Python 2.7
+#
+# Note: This code has been tested in Python 2.7
+
+
 import sys, getopt
 import os
 import re
 
+## Import MySQLdb
 try:
   import MySQLdb
 except:
   print "ERROR: \"MySQLdb\" module is not installed, please install and try again"
   sys.exit(0)
 
-# Exception Handling for LXML module
+## Importing Xpath for HTML Parsing 
 try:
   from lxml import html
   import lxml.html as LH
@@ -17,7 +38,7 @@ except:
   print "ERROR: \"lxml\" package is not installed, Please install and try again (For Python 3: sudo apt-get install python3-lxml) \n "
   sys.exit(0)
 
- 
+## Importing Requests module to access websites
 try:
   import requests
 except:
@@ -29,30 +50,41 @@ except:
 global debug 
 global gold_address   
 global silver_address 
-
-# Global Var initialization
-debug = 0
-gold_address = "https://www.investing.com/commodities/gold-historical-data"
-silver_address = "https://www.investing.com/commodities/silver-historical-data"
-
-
-## Initializating Database
+global self_obj 
 global db
 global cur
 
+# Global Var initialization
+debug = 0
+self_obj = {}
 
+self_obj["gold", "web"]    = "https://www.investing.com/commodities/gold-historical-data"
+self_obj["gold", "date"]   = "//div[@id=\"results_box\"]/table[@id=\"curr_table\"]/tbody/tr/td[1]/text()"
+self_obj["gold", "price"]  = "//div[@id=\"results_box\"]/table[@id=\"curr_table\"]/tbody/tr/td[2]/text()"
+
+self_obj["silver", "web"]   = "https://www.investing.com/commodities/silver-historical-data"
+self_obj["silver", "date"]  = "//div[@id=\"results_box\"]/table[@id=\"curr_table\"]/tbody/tr/td[1]/text()"
+self_obj["silver", "price"] = "//div[@id=\"results_box\"]/table[@id=\"curr_table\"]/tbody/tr/td[2]/text()"
+
+
+## Open Database 
 def openDB():
   global db
   global cur
   
-  db = MySQLdb.connect(host="localhost",  # your host 
-     user="root",	   # username
-     passwd="scu@123",     # password
-     db="goldSilverPrices")   # name of the database
- 
+  try:  
+    db = MySQLdb.connect(host="localhost",  # your host 
+         user="root",	   # username
+         passwd="scu@123",     # password
+         db="goldSilverPrices")   # name of the database
+  except:
+    print "ERROR: Couldn't connect to MySQLdb \"goldSilverPrices\", Flow terminating"
+    sys.exit(0)
+  
   # Create a Cursor object to execute queries.
   cur = db.cursor()
 
+## Delete Database
 def deleteDataDB():
   global db
   global cur
@@ -62,23 +94,27 @@ def deleteDataDB():
   ## Wipe table clean
   
   #query = "TRUNCATE TABLE gold"
-  query = "drop table if exists gold"
-  cur.execute(query)
+  try:
+    query = "drop table if exists gold"
+    cur.execute(query)
+    
+    query = "drop table if exists silver"
+    cur.execute(query)
   
-  query = "drop table if exists silver"
-  cur.execute(query)
-
-  ## 
-  query = "CREATE TABLE gold(date DATE, price VARCHAR(20))"
-  cur.execute(query)
-
-  ## 
-  query = "CREATE TABLE silver(date DATE, price VARCHAR(20))"
-  cur.execute(query)
+    ## 
+    query = "CREATE TABLE gold(date DATE, price VARCHAR(20))"
+    cur.execute(query)
+  
+    ## 
+    query = "CREATE TABLE silver(date DATE, price VARCHAR(20))"
+    cur.execute(query)
+  except:
+    print "ERROR: Couldn't "
 
   db.commit()  
   closeDB()
 
+## Close database
 def closeDB():
   global db
   global cur
@@ -86,12 +122,38 @@ def closeDB():
   db.commit()
   db.close()
 
-
+############################################
+##### Main Function ########################
+############################################
 def main():  
+  ## Parse Options
   parse_options()
+  
+  #Delete All Tables in Database
   deleteDataDB()
-  db = parse_websites(gold_address, "gold_data.txt", "gold")
-   
+  
+  # Open a new to build tables
+  openDB()
+  
+  # Parse Website Gold website
+  parse_websites("gold")
+  
+  # Parse Website
+  parse_websites("silver")
+  
+  # Close database object 
+  closeDB()
+  
+  # Exit Gracefully 
+  print "\n\nINFO: Flow completed successfully !!"
+
+
+############################################################
+## Description  : Transforms date format from website format 
+#		  to MySQL format
+## Input	: Date from website
+## Output	: Returns Date in MySQL format
+############################################################
 def format_date(date):
   date = re.sub(',','', date)
   month, day, year = re.split('\s+',date)
@@ -125,37 +187,60 @@ def format_date(date):
 
   return(year + "-" + month_no + "-" + day)  
 
-def parse_websites(web, db, tableName):  
-#  try:
-#    page=requests.get(web)
-#  except:
-#    print "ERROR: Website not available: ", web , 
-#
-#  tree = html.fromstring(page.content)
-    
+############################################################
+## Description  : Parse websites and extracts Gold/Silver 
+##		  Prices
+## Input	: Specify gold (or) silver
+## Output	: Builds Database with Price history 
+##		  Information
+############################################################
+def parse_websites(tableName):  
+  global debug
+  
+  web = self_obj[tableName, "web"] 
 
-  web = "/home/sudhiram/Desktop/gold.html"
-  with open(web, "r") as f:
-    page = f.read()
-  tree = html.fromstring(page)
+  print "INFO: Parsing HTML : ", web
 
-  openDB()
+  dateXpath   = self_obj[tableName, "date"]
+  priceXpath  = self_obj[tableName, "price"] 
+  
+  headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0'}
+ 
+  try:
+    page=requests.get(web, headers=headers)
+  except:
+    print "ERROR: Website not available: ", web 
+    sys.exit(0)
 
-  error = filter(lambda x: re.search(r'Error 403 You are banned', x), tree.xpath('//text()'))
-  if(error):
-    print "ERROR: You are banned from this site, Try again Later \n\t\t", web,
+  ## Check if the web data is sucessfully parsed
+  if(page.status_code != requests.codes.ok):
+    print "ERROR: Website Data was not available, The Website could be down or permissions might have been removed, Please check "
+    print "Message: ", page.content  
+    sys.exit(0)
 
-  date  = tree.xpath('//div[@id="results_box"]/table[@id="curr_table"]/tbody/tr/td[1]/text()')
-  price = tree.xpath('//div[@id="results_box"]/table[@id="curr_table"]/tbody/tr/td[2]/text()') 
+  tree = html.fromstring(page.content)    
+ 
+
+#  web = "/home/sudhiram/Desktop/gold.html"
+#  with open(web, "r") as f:
+#    page = f.read()
+#  tree = html.fromstring(page)
+
+#  error = filter(lambda x: re.search(r'Error', x, re.IGNORECASE), tree.xpath('//text()'))
+#  if(error):
+#    print "ERROR: Website is not available , Try again Later \n\t\t", web
+#    print "\n\n ", error
+
+  date  = tree.xpath(dateXpath)
+  price = tree.xpath(priceXpath) 
   
   for i in range(len(date)):
     new_date = format_date(date[i])
     query = "INSERT INTO " + tableName + " VALUES (\"" + new_date + "\",\""+ price[i] + "\")"
     cur.execute(query)
+    if(debug == 1):
+      print "[DEBUG] ", tableName, ", ", new_date , ":", price[i] 
   
-  closeDB()
-  return db
-
     
 def parse_options():     
   global debug 
@@ -172,10 +257,6 @@ def parse_options():
     elif("--debug" in key):
       debug = 1
   
-  print "INFO: Parsing HTML Files"
-
-
-
 ## Print Usage information to user and exit 
 def usage(): 
     print "\nUsage: " , sys.argv[0] , "  "
